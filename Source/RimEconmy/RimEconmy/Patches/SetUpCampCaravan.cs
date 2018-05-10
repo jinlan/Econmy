b8ccde3ecc2d5ec4aec7cd87f11b75e4622e70da
temple_wing@163.com <temple_wing@163.com>
2018-05-10 15:54:32 +0800

    fix a bug about set-up camp

--- Source/RimEconmy/RimEconmy/Patches/SetUpCampCaravan.cs
+++ Source/RimEconmy/RimEconmy/Patches/SetUpCampCaravan.cs
@@ -1,8 +1,8 @@
 ﻿using System;
+using System.Reflection;
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
-using Nandonalt_SetUpCamp;
 using Verse;
 using Verse.Sound;
 using RimWorld;
@@ -10,23 +10,26 @@ using RimWorld.Planet;
 using HugsLib.Utils;
 
 namespace RimEconmy.Patches.SetUpCamp {
-    public class SetUpCampCaravan : CaravanModded {
+    public class SetUpCampCaravan : Caravan {
+
+        private Gizmo setUpCampCommand;
+
+        public SetUpCampCaravan(){
+            Type type = Type.GetType("Nandonalt_SetUpCamp.CaravanCampUtility, Nandonalt_SetUpCamp, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null");
+            setUpCampCommand = (Gizmo)type.GetMethod("CampCommand", BindingFlags.Static | BindingFlags.Public).Invoke(null, BindingFlags.Static | BindingFlags.Public, Type.DefaultBinder, new object[]{this}, null);
+        }
+
         public override IEnumerable<Gizmo> GetGizmos() {
-            List<Gizmo> list = new List<Gizmo>();
-            Command_Action command_Action = new Command_Action();
-            command_Action.defaultLabel = "Set-Up Camp";
-            command_Action.defaultDesc = "Set-Up Camp";
-            command_Action.icon = SetUpCampTextures.CampCommandTex;
-            command_Action.action = delegate {
-                SoundDefOf.TickHigh.PlayOneShotOnCamera(null);
-                CaravanCampUtility.Camp(this);
-            };
             StringBuilder failReason = new StringBuilder();
             if(!TileFinder.IsValidTileForNewSettlement(this.Tile, failReason)) {
-                command_Action.Disable(failReason.ToString());
+                setUpCampCommand.Disable(failReason.ToString());
+            }else{
+                setUpCampCommand.disabled = false;
+                setUpCampCommand.disabledReason = null;
             }
-            list.Add(command_Action);
-            return base.GetGizmos().Concat(list);
+            List<Gizmo> list = base.GetGizmos().ToList();
+            list.Add(setUpCampCommand);
+            return list;
         }
     }
 }
